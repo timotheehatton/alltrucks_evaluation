55fd75f4861b2261121525465a59a3da371b889f
timothee.hatton <timothee.hatton@manomano.com>
2024-08-26 18:15:27 +0200

    strapi content

--- technician/views/account.py
+++ technician/views/account.py
@@ -6,16 +6,18 @@ from django.contrib import messages
 from users.decorators import technician_required
 from common.views.forms import LanguageForm
 from common.content.strapi import strapi_content
+from users.models import User
 
 
 @technician_required
 def index(request):
     page_content = strapi_content.get_content(page='account', parameters={'local': 'fr'})
-
+    manager = User.objects.filter(company=request.user.company, user_type='workshop').first()
     password_form = PasswordChangeForm(request.user)
     language_form = LanguageForm(instance=request.user)
     return render(request, 'workshop/account/index.html', {
         'password_form': password_form,
         'language_form': language_form,
-        'page_content': page_content['data']['attributes']
+        'page_content': page_content['data']['attributes'],
+        'manager': manager
     })
--- technician/views/stats.py
+++ technician/views/stats.py
@@ -4,22 +4,7 @@ from django.shortcuts import render
 from users.decorators import technician_required
 from users.models import Score
 from django.db.models import Max, Sum, ExpressionWrapper, IntegerField
-
-
-@technician_required
-def index(request):
-    user = request.user
-    last_datetime = Score.objects.filter(user=user).aggregate(date=Max('date'))['date']
-    scores_by_category = Score.objects.filter(
-        user=user, date=last_datetime
-    ).values('question_type').annotate(
-        total_score=Sum('score'),
-        success_percentage=ExpressionWrapper((Sum('score') * 100.0) / 20.0, output_field=IntegerField())
-    )
-
-    return render(request, 'technician/stats/index.html', {
-        'scores_by_category': scores_by_category
-    })
+from common.content.strapi import strapi_content
 
 
 @technician_required
@@ -38,3 +23,28 @@ def download_pdf(request):
     p.save()
 
     return response
+
+
+@technician_required
+def index(request):
+    page_content_categories = strapi_content.get_content(page='category', parameters={'local': 'fr'})
+    page_content = strapi_content.get_content(page='user-statistic', parameters={'local': 'fr'})
+    print(page_content)
+    last_datetime = Score.objects.filter(user=request.user).aggregate(date=Max('date'))['date']
+    scores_by_category = Score.objects.filter(user=request.user, date=last_datetime).values('question_type').annotate(
+        total_score=Sum('score'),
+        success_percentage=ExpressionWrapper((Sum('score') * 100.0) / 20.0, output_field=IntegerField())
+    )
+    scores_by_category = [
+        {
+            'question_type': page_content_categories['data']['attributes'][score['question_type']],
+            'total_score': score['total_score'],
+            'success_percentage': score['success_percentage']
+        }
+        for score in scores_by_category
+    ]
+
+    return render(request, 'technician/stats/index.html', {
+        'scores_by_category': scores_by_category,
+        'page_content': page_content['data']['attributes'],
+    })
