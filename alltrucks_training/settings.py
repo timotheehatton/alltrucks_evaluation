57534ff3cfed058cc3d4ad8c001b8fcf0bab1794
timothee.hatton <timothee.hatton@manomano.com>
2024-09-17 02:18:50 +0200

    change to postgre database

--- alltrucks_training/settings.py
+++ alltrucks_training/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os.path
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,10 +76,7 @@ WSGI_APPLICATION = 'alltrucks_training.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='postgres://localhost')
 }
 
 
