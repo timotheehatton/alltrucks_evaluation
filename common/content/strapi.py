bb1697261229f1c1aee5c2bed9ad872ba2fe1fc0
timothee.hatton <timothee.hatton@manomano.com>
2024-09-19 17:56:58 +0200

    change style chart

--- common/content/strapi.py
+++ common/content/strapi.py
@@ -1,8 +1,14 @@
 import requests
 
+from django.conf import settings
+
+
 class Content:
     def __init__(self):
-        self.api_url = "https://great-life-10c877bc60.strapiapp.com/api"
+        if settings.DEBUG:
+            self.api_url = "http://localhost:1337/api"  # Use localhost when in debug mode
+        else:
+            self.api_url = "https://great-life-10c877bc60.strapiapp.com/api"
 
     def get_content(self, pages, parameters):
         content = {}
